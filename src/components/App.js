688dc04af3eef530465b590ea7c7ec5954ad7a34
KIRTI-GITHUB <kirtirai7549@gmail.com>
2026-05-04 17:36:01 +0530

    App.js updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [text, setText] = useState("");
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <p>Enter your name:</p>
+      <br />
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      {text !== "" && <p>Hello, {text}!</p>}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
